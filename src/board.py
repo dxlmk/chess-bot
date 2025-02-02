da6d0c8ecf2eef31ebdc7730d854933060ef9070
Kleijne, D.L.M. (Daniël) <daniel.kleijne@ru.nl>
2025-02-02 21:58:50 +0100

    add pieces to board

--- src/board.py
+++ src/board.py
@@ -1,10 +1,13 @@
 import chess
 
-class Board:
+class ChessBoard:
     def __init__(self):
         self.board = chess.Board()
-        print(self.board.piece_at(36))
+        print(self.board)
+        print(self.board.piece_at(0))
 
-    def piece_at(self, row, col):
-        return self.board.piece_at(row+col)
-        
+    def get_piece_at(self, loc):
+        return self.board.piece_at(loc)
+    
+    def get_svg(self, size):
+        return chess.svg.board(board=self.board, size=480)
--- src/ui.py
+++ src/ui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from board import Board
+from board import ChessBoard
 import pygame
+import os
 
 # Window constants
 WINDOW_WIDTH = 480
@@ -13,11 +14,28 @@ GRAY = (224,213,234)
 WHITE  = (255,255,255)
 BLACK = (0,0,0)
 
+# Filepath for images
+PIECES_IMAGES = {
+    'K': 'images/wK.png', 'Q': 'images/wQ.png',
+    'R': 'images/wR.png', 'N': 'images/wN.png',
+    'B': 'images/wB.png', 'P': 'images/wP.png',
+    'k': 'images/bK.png', 'q': 'images/bQ.png',
+    'r': 'images/bR.png', 'n': 'images/bN.png',
+    'b': 'images/bB.png', 'p': 'images/bP.png'
+}
+
 class UI:
     def __init__(self, window):
         self.window = window
-        self.board = Board()
+        self.board = ChessBoard()
         self.font = pygame.font.Font(None, 48)
+
+        # Load the images for the chess pieces
+        self.symbols = {}
+        for piece, path in PIECES_IMAGES.items():
+            if os.path.exists(path):
+                self.symbols[piece] = pygame.image.load(path)
+                self.symbols[piece] = pygame.transform.scale(self.symbols[piece], (SQUARE_SIZE, SQUARE_SIZE))
 
     def run(self):
         # Run until the user quits the game
@@ -35,14 +53,23 @@ class UI:
         
     
     def draw_chess_board(self):
-        for col in range(8):
-            for row in range(8):
+        for row in range(8):
+            for col in range(8):
                 #Draw board
                 pygame.draw.rect(self.window, 
                                  GRAY if (row + col) % 2 == 0 else PURPLE, 
                                  (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 
                 #Draw pieces
+                piece = self.board.get_piece_at((7 - row) * 8 + col)
+                
+                if piece:
+                    piece_symbol = piece.symbol()
+                    if piece_symbol in self.symbols:
+                        # Draw the piece on the square
+                        self.window.blit(self.symbols[piece_symbol], 
+                                         (col * SQUARE_SIZE, row * SQUARE_SIZE))
+
 
     
 def start_ui():
