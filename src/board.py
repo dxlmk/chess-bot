413b58c671b27bd5d53fedf2d2e7d151764f60f8
Kleijne, D.L.M. (Daniël) <daniel.kleijne@ru.nl>
2025-02-03 19:08:56 +0100

    add highlight and move functionality

--- src/board.py
+++ src/board.py
@@ -3,11 +3,14 @@ import chess
 class ChessBoard:
     def __init__(self):
         self.board = chess.Board()
-        print(self.board)
-        print(self.board.piece_at(0))
 
     def get_piece_at(self, loc):
         return self.board.piece_at(loc)
     
     def get_svg(self, size):
         return chess.svg.board(board=self.board, size=480)
+    
+    def make_move(self, pos, new_pos):
+        move = chess.Move(pos, new_pos)
+        if move in self.board.legal_moves:
+            self.board.push(move)
--- src/ui.py
+++ src/ui.py
@@ -13,6 +13,7 @@ PURPLE = (149,122,176)
 GRAY = (224,213,234)
 WHITE  = (255,255,255)
 BLACK = (0,0,0)
+GREEN = (124,149,132)
 
 # Filepath for images
 PIECES_IMAGES = {
@@ -29,6 +30,7 @@ class UI:
         self.window = window
         self.board = ChessBoard()
         self.font = pygame.font.Font(None, 48)
+        self.selected_square = None
 
         # Load the images for the chess pieces
         self.symbols = {}
@@ -42,33 +44,57 @@ class UI:
         running = True
         while running:
             self.window.fill((0, 0, 0))
-            self.draw_chess_board()
+            self.draw_board()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_click(pygame.mouse.get_pos())
             
             pygame.display.flip()
         pygame.quit()
         
+    def handle_click(self, mouse_pos):
+        col = mouse_pos[0] // SQUARE_SIZE
+        row = mouse_pos[1] // SQUARE_SIZE
+
+
+
+        if self.selected_square:
+            # If a piece is already selected, attempt the move
+            pos = self.selected_square[0]*8 + self.selected_square[1]
+            new_pos = (7-row)*8 + col
+            self.board.make_move(pos, new_pos)
+            self.selected_square = None  # Deselect after move
+        else:
+            # Select the clicked square
+            self.selected_square = (7-row, col)
+
+
     
-    def draw_chess_board(self):
+
+    def draw_board(self):
         for row in range(8):
             for col in range(8):
                 #Draw board
                 pygame.draw.rect(self.window, 
                                  GRAY if (row + col) % 2 == 0 else PURPLE, 
-                                 (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                                 (col * SQUARE_SIZE, (7-row) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
                 
-                #Draw pieces
-                piece = self.board.get_piece_at((7 - row) * 8 + col)
+                if self.selected_square == (row, col):
+                    pygame.draw.rect(self.window, GREEN, 
+                                     (col * SQUARE_SIZE, (7-row) * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                
+                #Draw pieces 
+                piece = self.board.get_piece_at(row * 8 + col)
                 
                 if piece:
                     piece_symbol = piece.symbol()
                     if piece_symbol in self.symbols:
                         # Draw the piece on the square
                         self.window.blit(self.symbols[piece_symbol], 
-                                         (col * SQUARE_SIZE, row * SQUARE_SIZE))
+                                         (col * SQUARE_SIZE, (7-row) * SQUARE_SIZE))
 
 
     
